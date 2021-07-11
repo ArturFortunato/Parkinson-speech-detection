22568262e69b9b3dd671370e29e7b7f082a4109b
ArturFortunato <artur.fortunato1@gmail.com>
2021-07-11 14:59:44 +0100

    Added feature_extaction again

---
+++ feature_extraction/paths.py
@@ -0,0 +1,8 @@
+FRALUSOPARK_AUDIOS = '/afs/inesc-id.pt/home/aof/thesis/fralusopark_wav'
+FRALUSOPARK_OUTPUT = '/afs/inesc-id.pt/home/aof/thesis/features/fralusopark'
+
+MDVR_KCL_AUDIOS = '/afs/inesc-id.pt/home/aof/thesis/mdvr_kcl_wav'
+MDVR_KCL_OUTPUT = '/afs/inesc-id.pt/home/aof/thesis/features/mdvr_kcl'
+
+GITA_AUDIOS = '/afs/inesc-id.pt/corpora/pc-gita/PC-GITA_per_task_44100Hz/read_text/ayerfuialmedico/sin_normalizar'
+GITA_OUTPUT = '/afs/inesc-id.pt/home/aof/thesis/features/gita'
