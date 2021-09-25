69c1082897a845c66429e80edbde9e62ee5a4862
ArturFortunato <artur.fortunato1@gmail.com>
2021-09-25 18:41:44 +0100

    Added radar graph images to the document

--- explainability/radar_chart.py
+++ explainability/radar_chart.py
@@ -6,6 +6,7 @@ BASE_PATH = '../dissertation2/csvs'
 EXPERIMENTS = ['baseline', 'baseline_200', 'semi', 'semi_200', 'independent', 'independent_200']
 RESULT_COLUMNS = ['fscore', 'specificity', 'accuracy', 'recall', 'precision']
 COLORS = ['C1', 'C2', 'C3', 'C4', 'C5']
+YTICKS = [0.0, 0.2, 0.4, 0.6, 0.8, 1.0]
 
 def generate_radar_chart(experiment):
 
@@ -26,6 +27,9 @@ def generate_radar_chart(experiment):
         ax.plot(theta, values, color=COLORS[row], marker="o", label="Name of {}".format(row))
         plt.xticks(theta[:-1], [col if col != 'fscore' else 'f1-score' for col in RESULT_COLUMNS], color=COLORS[row], size=12)
         ax.tick_params(pad=15) # to increase the distance of the labels to the plot
+
+        ax.set_yticks(YTICKS)
+        ax.set_yticklabels(map(str, YTICKS))
 
     # plt.title('{} experiment results'.format(experiment.replace('_', ' ')))
     plt.savefig('charts/{}_radar.jpg'.format(experiment))
